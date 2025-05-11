97ef4e7407864779fb0110af6f81045c134160ad
Alejandra Sandoval <pa.sandoval@uniandes.edu.co>
2025-05-11 18:02:01 -0500

    fix: se obtiene el icono desde url

--- app/src/main/java/com/example/vinilos/ui/components/common/CircleIcon.kt
+++ app/src/main/java/com/example/vinilos/ui/components/common/CircleIcon.kt
@@ -2,24 +2,18 @@ package com.example.vinilos.ui.components.common
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.example.vinilos.R
-import androidx.compose.ui.graphics.Color
-
+import coil.compose.AsyncImage
 
 @Composable
 fun CircleIcon() {
-    Icon(
-        painter = painterResource(R.drawable.app_icon_round),
+    AsyncImage(
+        model = "https://drive.google.com/uc?export=download&id=1BIzP6UTcoLLbiuCZHQDfEftWDvaKBwSd",
         contentDescription = "Icono",
-        tint = Color.Unspecified,
         modifier = Modifier
             .size(50.dp)
             .padding(start = 0.dp, top = 0.dp, end = 10.dp, bottom = 0.dp)
-
     )
 }
